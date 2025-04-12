d573e22ee5fac9ac97eef44928690f579d9f30f8
codeurient <codeurient@gmail.com>
2025-04-12 11:51:50 +0400

    im pro comment

--- data/importProduct.js
+++ data/importProduct.js
@@ -2,7 +2,16 @@ const mysql = require("mysql2/promise");
 const fs = require("fs");
 
 async function insertProducts() {
+  // Замените следующие значения на реальные параметры подключения к вашей MySQL базе данных
+  // const connection = await mysql.createConnection({
+  //   host: "127.0.0.1",
+  //   port: "3306",
+  //   user: "root",
+  //   password: "root1234@",
+  //   database: "web_shop",
+  // });
 
+  //   await connection.execute("TRUNCATE TABLE products");
 
   let rawdata = fs.readFileSync("data/products.json");
   let products = JSON.parse(rawdata);
