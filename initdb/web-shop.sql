8ea6bc38aafae7ff41e7586d0babb4cef12f7d15
codeurient <codeurient@gmail.com>
2025-04-12 11:53:22 +0400

    cart item

--- initdb/web-shop.sql
+++ initdb/web-shop.sql
@@ -40,3 +40,33 @@ CREATE TABLE `card_info` (
 --
 -- Dumping data for table `card_info`
 --
+
+LOCK TABLES `card_info` WRITE;
+/*!40000 ALTER TABLE `card_info` DISABLE KEYS */;
+INSERT INTO `card_info` VALUES (1,'VISA',12,26,123,1846.00,'valid','8820354696467284'),(2,'MasterCard',12,26,456,1597.00,'valid','5248106661644884'),(3,'VISA',1,20,789,2000.00,'expired','4340511554108849'),(4,'MasterCard',1,20,101,2000.00,'expired20','0307328035514696'),(5,'VISA',6,26,234,2000.00,'stolen','0779330784258313'),(6,'MasterCard',6,26,567,2000.00,'stolen','1151842195999505'),(7,'VISA',12,26,890,0.00,'valid','7178218557247775'),(8,'MasterCard',12,26,112,0.00,'valid','3320643190265792'),(9,'VISA',12,26,0,2000.00,'blocked','9181347306820824'),(10,'MasterCard',12,26,0,2000.00,'blocked','6107972359241284');
+/*!40000 ALTER TABLE `card_info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart_items`
+--
+
+DROP TABLE IF EXISTS `cart_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart_items` (
+  `cart_item_id` smallint NOT NULL AUTO_INCREMENT,
+  `user_id` smallint DEFAULT NULL,
+  `product_id` smallint DEFAULT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`cart_item_id`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `cart_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `cart_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=982 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart_items`
+--
